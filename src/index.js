f98cbeb26eef685238fcbc85bb78dd961f80755e
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-14 15:33:30 +0200

    Sprint 1 : 1) Supprimer les fichiers créés par défaut dans le dossier src. 2) Créer le fichier index.js pour initialiser le projet

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+// Importer les bibliothèques
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Créer du composant
+// Approche basée sur la fonction.
+const App = () => { // Sélecteur du projet
+    return <div>Comments</div>;
+}
+
+// Afficher à l'écran
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>, // Sélecteur du projet créé au dessus.
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
