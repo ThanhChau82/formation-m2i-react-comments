8cf067193f6ceda2fa93a2dfe9126e48a2dfe21c
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-14 17:21:24 +0200

    Sprint 2 : Créer le premier composant réutilisable CommentDetail

---
+++ src/CommentDetail.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//import faker from 'faker';
+
+const CommentDetail = props => {
+    console.log(props);
+    return (
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img src=""/>                
+            </a>
+            <div className="content">
+                <a className="author">{props.author}</a>
+                <div className="metadata">
+                    <div className="date">{props.date_publication}</div>
+                    <div className="rating">
+                        <i className="star icon"></i>
+                        {props.rating} Faves
+                    </div>
+                </div>
+
+                <div className="text">
+                    {props.text}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 // Importer les bibliothèques
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 // Créer du composant
 // Approche basée sur la fonction.
 const App = () => { // Sélecteur du projet
-    return <div>Comments</div>;
+    return (
+        <div className="ui comments">
+            <CommentDetail author="Sam" date_publication="11/06/2021" rating="4" text="Commentaire de Sam"/>
+            <CommentDetail author="Julien" date_publication="12/06/2021" rating="5" text="Commentaire de Julien"/>
+            <CommentDetail author="Marie" date_publication="13/06/2021" rating="3" text="Commentaire de Marie"/>
+        </div>
+    );
 }
 
 // Afficher à l'écran
