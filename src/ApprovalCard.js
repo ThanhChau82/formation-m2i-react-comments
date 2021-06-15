9c772d26b84d06298cdc64b27085f7af02048a1b
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-15 09:55:34 +0200

    Sprint 2 - Ajouter le template Semantic UI pour ApprovalCard

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const ApprovalCard = () => {
     return (
-        <div>ApprovalCard</div>
+        <div className="ui cards">
+            <div className="card">
+                <div className="content">
+                    <div>ApprovalCard</div>
+                </div>
+
+                <div className="extra content">
+                    <div className="ui two buttons">
+                        <div className="ui basic green button">Approve</div>
+                        <div className="ui basic red button">Decline</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
